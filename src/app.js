e8d423c5bdf1b336730596d2eb230e3a66d32b77
arthurguerraa <127884471+arthurguerraa@users.noreply.github.com>
2026-08-15 21:16:47 -0300

    Instalando o CORS e projeto rodando

--- src/app.js
+++ src/app.js
@@ -1,30 +1,46 @@
 const express = require('express');
+const cors = require('cors'); // Importado aqui no topo
 require('dotenv').config();
 require('./config/database');
 
 const app = express();
 
+// Configurações do Express
+app.use(cors()); // Libera o CORS para o seu frontend funcionar!
 app.use(express.json());
 
-// teste se o servidor está rodando
+// Teste se o servidor está rodando
 app.get('/', (req, res) => {
   res.json({ message: 'CineTrack API rodando!' });
 });
 
-const tratarErros = require('./middlewares/errorMiddleware');
+// Rotas do Sistema
 const authRoutes = require('./routes/authRoutes');
 app.use('/auth', authRoutes);
+
 const filmeRoutes = require('./routes/filmeRoutes');
 app.use('/filmes', filmeRoutes);
+
 const avaliacaoRoutes = require('./routes/avaliacaoRoutes');
 app.use('/avaliacoes', avaliacaoRoutes);
+
 const listaRoutes = require('./routes/listaRoutes');
 app.use('/listas', listaRoutes);
+
 const rankingRoutes = require('./routes/rankingRoutes');
 app.use('/ranking', rankingRoutes);
+
 const generoRoutes = require('./routes/generoRoutes');
 app.use('/generos', generoRoutes);
-app.use(tratarErros); // sempre por último
 
+// Tratamento de erros (Sempre depois das rotas e antes do module.exports)
+const tratarErros = require('./middlewares/errorMiddleware');
+app.use(tratarErros);
+
+// FAÇA O SERVIDOR RODAR NA PORTA 3000
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`🚀 Servidor rodando com sucesso na porta ${PORT}!`);
+});
 
 module.exports = app;
